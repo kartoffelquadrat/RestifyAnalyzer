42444d8c0be021e36763a6569d3e9e7632005465
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-07-27 12:00:15 -0400

    added control loop for automated testing of manual code

--- analyze.sh
+++ analyze.sh
@@ -14,12 +14,59 @@ function getCodeName
     CODENAME=$GROUP-$ANIMAL
 }
 
+function analyzeManual
+{
+	# Red Manual: Xox
+	# Green Manual: BookStore
+	# Blue Manual: BookStore
+	# Yellow Manual: Xox
+	case $GROUP in
+
+        	Red )
+		MANUAL=XoxInternals
+		;;
+
+        	Green )
+		MANUAL=BookStoreInternals
+		;;
+
+        	Blue )
+		MANUAL=BookStoreInternals
+		;;
+
+        	Yellow )
+		MANUAL=XoxInternals
+		;;
+
+	esac
+
+	echo -n "   * Testing $MANUAL... "
+	cd $UPLOADDIR
+
+	# Verify upload exists
+	if [ ! -d $CODENAME-File-Upload/$MANUAL ]; then
+		echo "Upload not found, skipping"	
+		echo " * Manual: MISSING" >> $BASEDIR/$REPORT
+		
+	else
+		cd $CODENAME-File-Upload/$MANUAL
+		mvn -q clean package spring-boot:run &
+		sleep 15
+		pkill -9 java
+		cd -
+		echo OK
+	fi
+}
+
 function analyzeUpload
 {
     getCodeName $1
-    echo "Analyzing $CODENAME"
+    echo " > Analyzing $CODENAME"
     cd $BASEDIR
     echo "# $CODENAME" >> $REPORT
+
+    ## Analyze the manual submission
+    analyzeManual
 }
 
 ## Main logic
